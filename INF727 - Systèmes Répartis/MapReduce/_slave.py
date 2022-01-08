b18e816e286450c2d5ddbe8ce9c556cb81e3fabd
SaraBoutigny <saraboutigny@gmail.com>
2022-01-08 14:17:00 +0100

    Update _slave.py

--- INF727 - Systèmes Répartis/MapReduce/_slave.py
+++ INF727 - Systèmes Répartis/MapReduce/_slave.py
@@ -32,30 +32,6 @@ if not os.path.exists('/tmp/{}/shuffles'.format(login)): os.mkdir('/tmp/{}/shuff
 if not os.path.exists('/tmp/{}/shuffles_received'.format(login)): os.mkdir('/tmp/{}/shuffles_received'.format(login))
 if not os.path.exists('/tmp/{}/partial_wordcount'.format(login)): os.mkdir('/tmp/{}/partial_wordcount'.format(login))
 
-# FONCTIONS ===========================================================================================================
-
-def slave():
-    # Récupération des splits
-    for machine in dict_machines.values():
-
-        # Récupération du nom du fichier txt dans le dossier splits
-        _, split_name, _, _ =  machine.execute("ssh {}@{} ls /tmp/{}/splits/".format(machine.login,machine.name,machine.login),print_out=True)
-
-        # Récupération du contenu du fichier split Sx.txt
-        _, out, _, _ = machine.execute("ssh {}@{} cat /tmp/{}/splits/{}".format(machine.login,machine.name,machine.login,split_name),return_out=True,print_out=True)
-
-        # Création du dossier maps
-        machine.execute("ssh {}@{} mkdir /tmp/{}/maps".format(machine.login, machine.name, machine.login),print_out=True)
-
-        # Création de l'unsorted map UMx.txt
-        machine.execute("ssh {}@{} touch /tmp/{}/maps/{}".format(machine.login, machine.name, machine.login, split_name.replace("S","UM")),print_out=True)
-
-        # Remplissage de l'unsorted map UMx.txt
-        machine.execute('ssh {}@{} echo {} > /tmp/{}/maps/{}'.format(machine.login, machine.name, mapping(out), machine.login, split_name.replace("S","UM")),print_out=True)
-
-        machine.execute("ssh {}@{} cat /tmp/{}/maps/{}".format(machine.login, machine.name, machine.login, split_name.replace("S","UM")),print_out=True)
-
-
 # ==================================================================================================================== #
 #                                                      MAP                                                             #
 # ==================================================================================================================== #
